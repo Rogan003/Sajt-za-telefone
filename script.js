41054a2be445fe880e6b5352178e62bad617d054
Rogan003 <roganovic.veselin@jjzmaj.edu.rs>
2020-12-05 22:16:23 +0100

    Odradjen deo JS i JQuery-ja, ali bez provere da li radi

---
+++ script.js
@@ -0,0 +1,142 @@
+var telefoni = [
+    {
+        "id" : 1,
+        "basicData" : {
+            "model" : "Samsing Galaxy A70",
+            "brand" : "Samsung",
+            "phoneImage" : "samsung.jpg",
+            "price" : "39 000 / 330"
+        },
+        "specs" : {
+            "memory" : "Interna: 128 GB, RAM: 6GB",
+            "battery" : "Neizmenjiva, Li-Po 4500mAh, punjač 25W",
+            "camera" : "Glavna: 32 Mpx, Siroka: 8 Mpx, Dubinska: 5 Mpx ; Selfi: 32 Mpx"
+        },
+        "review" : [
+            {
+                "reviewtext" : [
+                    {
+                        "heading" : "Memorija",
+                        "text" : ["Uređaj ima 6 GB RAM-a, 128 GB interne memorije. To je vrlo dobro, a u teoriji ne morate ni ubaciti memorijsku karticu obzirom na količinu interne memorije, iako postoji slot u koji može da se ubaci kartica kapaciteta do 512 GB. Brzina memorije je odlična, što daje dobre rezultate u kombinaciji sa čipsetom. Ovo je brz pametni telefon, a u svojoj klasi pokazuje dobre rezultate - nema kočenja."],
+                        "picture" : "samsungmemorija.jpg",
+                        "alt" : "Uporedjivanje memorijskih performansi",
+                        "caption" : "Uporedjivanje memorijskih performansi"
+                    },
+                    {
+                        "heading" : "Baterija",
+                        "text" : [
+                        "Pametni telefon ima ugrađenu Li-Pol bateriju kapaciteta 4500 mAh, podržava brzo punjenje, a punjač od 25W je uključen u prodajno pakovanje. Ovo je prvi Samsung uređaj sa takvom tehnologijom punjenja, ukupno vreme punjenja baterije je oko 65 minuta. Nema bežičnog punjenja, ali to je normalno za ovaj segment.",
+                        "Svaki model ima svoj profil rada, sopstvene aplikacije. A ako je sa A50 sa baterijom od 4000 mAh i Exynos čipsetom bilo moguće ostvariti otprilike dva dana 7-7,5 sati sa rada ekrana, tako su ovde pokazatelji rasli. Dva i po, čak i tri dana, uređaj može da radi uz manje zahtevnu upotrebu, ako ga ne opterećujete igrama, imate uobičajeni set softvera. Za one koji uređaj koriste intenzivnije, to može biti 1,5-2 dana, igrači mogu da računaju na oko deset sati rada, što je takođe i neka vrsta rekorda, imajući u vidu dijagonalu ekrana. A ovo je veliki plus za ovaj model.",
+                        ],
+                        "picture" : "samsungbaterija.jpg",
+                        "caption" : "Punjac"
+                    },
+                    {
+                        "heading" : "Kamera",
+                        "text" : [
+                        "Interfejs kamere je poznat sa ostalih Samsungovih modela, nema ničega novog. Prisutan je režim Pro, AR Emoji i drugi efekti.",
+                        "Prednju kameru karakteriše matrica rezolucije 32 MP (f / 2.0), ima bokeh efekat, razne dodatne efekte, nema autofokusa.",
+                        "Glavna kamera se sastoji od tri modula - 32 MP (f / 1.7) + 8 MP (f / 2.2) + 5 MP (f / 2.2), (širokougaona kamera, konvencionalna kamera, kamere za efekat dubine polja). Opcije uključuju LED blic, QHD video snimanje. Širokougaona kamera ima ugao snimanja od 123 stepena (bez automatskog fokusiranja), konvencionalna kamera - 78 stepeni. Po našem mišljenju, glavna kamera dobro snima kada je sunčan dan, u zatvorenom se već pojavljuju šumovi. Ali u segmentu kojem uređaj pripada ovo nije loš kvalitet.",
+                        ],
+                        "picture" : "samsungkamera.jpg",
+                        "caption" : "Kamera"
+                    },
+                    {
+                        "heading" : "Utisci",
+                        "text" : [
+                        "Po kvalitetu komunikacije, jačini poziva, nema nikakvih problema, sve je na odgovarajućem nivou. Cena A70 je za nekih 20 odsto viša od cene modela A50 6/128 GB. Koliko je ta cena adekvatna? Verovatno je potrebno da pogledate direktne konkurente da biste procenili ovaj parametar. Odjednom se ispostavilo da sa sličnim kapacitetom memorije i uporedivom dijagonalom na tržištu nema mnogo ponuda. Počećemo sa Honor View 20. Ovaj model nema slot memorijske kartice, poseduje IPS matricu (za nekoga, možda je to plus, većina ipak bira AMOLED). Veličina ekrana je 6,4 inča. Reč je o prijatnom uređaju, cene su slične.",
+                        "Tu je i Xiaomi Mi 9T, ovaj uređaj takođe ima dijagonalu od 6,39 inča, ali poseduje SuperAMOLED ekran. Senzor otiska prsta ugrađen je u ekran. Veoma simpatičan model, na koji vredi obratiti pažnju.",
+                        "Ali nabrajajući konkurente, uvek smo dolazili do zaključka da su oni konkurenti modelu A50, jer su njemu slični i imaju dijagonalu od oko 6,4 inča. Ali nije bilo direktnih konkurenata za A70 sa takvom dijagonalom. I tu dolazimo do paradoksalnog zaključka - A50 je direktan konkurent A70. Ovi modeli su slični, ali A70 nudi najveću moguću dijagonalu trenutno i po adekvatnoj ceni. Prednost u odnosu na A50 je vreme rada, koje je znatno produženo. Prednost je i brzo punjenje uz punjač od 25 W. Nedostaci se odnose na ergonomiju i dimenzije uređaja. I ovde je teško proceniti šta će tačno biti bolje za vas. Većina ljudi kupuje A50, ovo je bestseler bez ikakvih popusta. Ali A70 nije toliko popularan, jer košta nešto više. Ali ako vam je potreban veći ekran, onda ima smisla platiti ovaj novac, razlika se isplati.",
+                        ],
+                        "picture" : "samsung2.jpg",
+                        "caption" : "Izgled telefona i dostupne boje",
+                    }
+                ],
+                "reviewauthor" : "Mobilni Svet",
+                "dugme" : "Prikazi/Sakrij",
+                "naziv" : "Review 1"
+            },
+            {
+                "reviewtext" : [
+                    {
+                        "heading" : "Baterija",
+                        "text" : ["Galaxy A70 ima bateriju od 4.500 mAh (tipično) i uz nju možeš da gledaš više video sadržaja i to mnogo, mnogo duže. Efikasna tehnologija superbrzog punjenje od 25 W znači da je baterija puna u tren oka i već možeš da kreneš. Tvoja nova maksima: puni manje, obavi više."],
+                        "picture" : "samsungbaterija2.webp",
+                        "caption" : "Baterija"
+                    },
+                    {
+                        "heading" : "Kamera",
+                        "text" : [
+                        "Tri kamere telefona Galaxy A70 obuhvataju ultraširoku kameru koja pruža mogućnost slikanja u vidnom polju širine 123 stepena, poput ljudskog oka, kao i kameru od 32 MP (F1,7) za slikanje jasnih i dobro osvetljenih fotografija tokom celog dana. Treća je dubinska kamera od 5 MP koja prilagođava žižnu daljinu.",
+                        "Kad snimaš selfije, ti si u centru pažnje. Uz prednju kameru od 32 MP, fotografije su svetlije i jasnije bez obzira na doba dana kada slikaš. Drugim rečima, uvek si u najboljem izdanju! Koristi fokus selfija za prilagođavanje dubine i zamućenosti pozadine.",
+                        ],
+                        "picture" : "samsungkamera2.webp",
+                        "caption" : "Kamera"
+                    },
+                    {
+                        "heading" : "Dizajn",
+                        "text" : ["Ugrađeni senzori daju uređaju minimalistički izgled. Osim toga, Galaxy A70 ima senzor otiska prsta na ekranu. Zadnja površina sa oblogom 3D Glasstic dobro leži u ruci bez obzira da li krećeš na posao ili na večeru. Izaberi boju po sopstvenoj želji: crna, bela, plava ili moderna koralna nijansa."],
+                        "picture" : "samsungdizajn.webp",
+                        "caption" : "Dizajn"
+                    }
+                ],
+                "reviewauthor" : "Samsung Srbija",
+                "dugme" : "Prikazi/Sakrij",
+                "naziv" : "Review 1"
+            },
+            {
+                "reviewauthor" : "Benchmark",
+                "reviewurl" : "https://www.youtube.com/embed/nGh8jM1f218",
+                "naziv" : "Review YT"
+            }
+        ],
+        "experiences" : [
+            {
+                "id" : 1,
+                "authorname" : "Username001",
+                "rating" : 5,
+                "text" : "Vrlo dobar telefon, sa vrlo kvalitetnim karakteristikama.",
+                "likes" : 5,
+                "dislikes" : 2,
+                "comments" : [
+                    {
+                        "id" : 14,
+                        "authorname" : "Someone",
+                        "text" : "Slazem se.",
+                    }
+                ]
+            },
+            {
+                "id" : 3,
+                "authorname" : "ProsecniKorisnik",
+                "rating" : 4,
+                "text" : "Odlican telefon za te pare, ali je napravljen od vrlo nekvalitetnih materijala.",
+                "likes" : 10,
+                "dislikes" : 7,
+                "comments" : []
+            }
+        ]
+    }
+]
+function loadPage(n)
+{
+    $("h2").append(telefoni[n].basicData.model);
+    $("#specifikacije thead").append("<tr><th>Memorija</th><th>Baterija</th><th>Kamera</th></tr>");
+    $("#specifikacije tbody").append("<tr>" + "<td>" + telefoni[n].specs.memory + "</td>" + "<td>" + telefoni[n].specs.battery + "</td>" + "<td>" + telefoni[n].specs.camera + "</td>" +"</tr>");
+    $("section").each(function(i){
+        $("h3")[i].append(telefoni[n].review[i].naziv);
+        $("h3")[i].after("Autor: " + telefoni[n].review[i].reviewauthor);
+        $.each(telefoni[n].review[i].reviewtext, function(j){
+            $("div div div")[j].append("<h4>" + telefoni[n].review[i].reviewtext[j].heading + "</h4>");
+            $.each(telefoni[n].review[i].reviewtext.text, function(k){
+                $("div div div *:last").after("<p>" + telefoni[n].review[i].reviewtext[j].text[k] +"</p>");
+            });
+            $("figure img")[j].src(telefoni[n].review[i].reviewtext[j].picture);
+            $("figure img")[j].alt(telefoni[n].review[i].reviewtext[j].alt);
+            $("figure img")[j].after("<figcaption>" + telefoni[n].review[i].reviewtext[j].caption + "</figcaption>");
+        });
+        $("section button")[i].append(telefoni[n].review[i].dugme);
+    });
+}
+$(document).ready();
+//Odradjeno nesto, nmp da li radi, treba jos izbacivati stvari po stranici i testirati
